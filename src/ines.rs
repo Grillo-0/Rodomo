bc0027ef4916e3dc9ef8027e2db17b887110c411
Arthur Grillo <arthurgrillo@riseup.net>
2024-01-13 14:54:36 -0300

    src/ines: Correct the program ROM offset by the presence of the trainer
    On the iNES file format, there may be a 'trainer' section before the
    program ROM, take into consideration this sections when calculating the
    program_rom_offset.

--- src/ines.rs
+++ src/ines.rs
@@ -7,11 +7,15 @@ pub struct INes {
 
 impl INes {
     pub fn parse(path: &str) -> INes {
+        const TRAINER_MASK: u8 = 1 << 2;
+
         let bytes = fs::read(path).expect("could not read file!");
         assert_eq!(String::from_utf8_lossy(&bytes[0..3]), "NES");
 
+        let flags_6 = bytes[6];
+
         let program_size = 16 * (1 << 10) * bytes[4] as usize;
-        let program_rom_offset = 16;
+        let program_rom_offset = 16 + 512 * (flags_6 & TRAINER_MASK) as usize;
 
         INes {
             program: bytes[(program_rom_offset)..(program_rom_offset + program_size)].to_vec(),
