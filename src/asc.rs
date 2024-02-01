65a87abd26b502febb13bf4bf330c59614c24129
Arthur Grillo <arthurgrillo@riseup.net>
2024-02-01 16:25:01 -0300

    src/asc: Warn if an address is accessed with no device registered
    Currently, we panic if an address is accessed but no MemoryMapped device
    is registered to it. This will cause a panic on the majority of games,
    as we don't implement all the devices that as NES has(just the memory,
    CPU, and PPU).
    
    To fix that, just warns instead of panicking if an address is accessed
    with no device mapped to it.

--- src/asc.rs
+++ src/asc.rs
@@ -36,28 +36,30 @@ impl Asc {
 
 impl MemoryMapped for Asc {
     fn write(&mut self, addr: u16, value: u8) {
-        self.devices
-            .get_mut(&addr)
-            .unwrap_or_else(|| {
-                panic!(
-                    "tried to write value {:#x} to address {:#x} that no device is registred",
-                    value, addr
-                )
-            })
-            .borrow_mut()
-            .write(addr, value);
+        let dev = self.devices.get_mut(&addr);
+
+        if let Some(dev) = dev {
+            dev.borrow_mut().write(addr, value);
+        } else {
+            eprintln!(
+                "[WARN]: tried to write value {:#x} to address {:#x} that no device is registred",
+                value, addr
+            );
+        }
     }
 
     fn read(&mut self, addr: u16) -> u8 {
-        self.devices
-            .get_mut(&addr)
-            .unwrap_or_else(|| {
-                panic!(
-                    "tried to read from address {:#x} that no device is registred",
-                    addr
-                )
-            })
-            .borrow_mut()
-            .read(addr)
+        let dev = self.devices.get_mut(&addr);
+
+        if let Some(dev) = dev {
+            let value = dev.borrow_mut().read(addr);
+            value
+        } else {
+            eprintln!(
+                "[WARN]: Tried to read from address {:#x} that no device is registred",
+                addr
+            );
+            0
+        }
     }
 }
